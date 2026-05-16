08c26afb9fe882001cc450153f89c4376b7efbe6
minhduc <duc.nm1022@gmail.com>
2026-05-16 15:35:08 +0700

    feat lession 2 homework part2

---
+++ tests/lesson-02/ex1.js
@@ -0,0 +1,6 @@
+let name = "my number";
+console.log(name);
+const number = 12;
+console.log(number);
+let isEven = false;
+console.log(isEven);
---
+++ tests/lesson-02/ex3.js
@@ -0,0 +1,5 @@
+const a = true;
+const b = false;
+const c = true;
+console.log(a && b && c);
+console.log(a && b || c);
